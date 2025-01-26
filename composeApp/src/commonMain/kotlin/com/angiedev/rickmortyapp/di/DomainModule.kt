db0bb6ca51446fb4763cbaab71045f599efff8cb
Angie <angelesmafon@gmail.com>
2025-01-26 18:45:30 +0100

    feat: first use case and di

--- composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/di/DomainModule.kt
@@ -1,7 +1,9 @@
 package com.angiedev.rickmortyapp.di
 
+import com.angiedev.rickmortyapp.domain.GetRandomCharacterUseCase
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val domainModule = module {
-
+    factoryOf(::GetRandomCharacterUseCase)
 }
