f23a4e60f0a7e142ba3541e0aa2b5e1e82301541
Angie <angelesmafon@gmail.com>
2025-01-26 21:23:57 +0100

    feat: Character of the day part 1

--- composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/ui/home/homebottombar/tabs/characters/CharactersScreen.kt
+++ composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/ui/home/homebottombar/tabs/characters/CharactersScreen.kt
@@ -1,13 +1,30 @@
 package com.angiedev.rickmortyapp.ui.home.homebottombar.tabs.characters
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.angiedev.rickmortyapp.domain.model.CharacterModel
+import com.angiedev.rickmortyapp.ui.extensions.vertical
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 
@@ -18,10 +35,67 @@ fun CharactersScreen() {
     val charactersViewModel = koinViewModel<CharactersViewModel>()
     val state = charactersViewModel.characterOfTheDayState.collectAsStateWithLifecycle()
 
-   Column(modifier = Modifier.fillMaxSize().background(Color.Red)) {
-        Text(text = "Characters Screen: ${state.value.characterOfTheDay?.id}")
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CharacterOfTheDayComponent(state.value.characterOfTheDay)
     }
 }
+
+
+@Composable
+fun CharacterOfTheDayComponent(characterOfTheDay: CharacterModel? = null) {
+
+        Card(
+            modifier = Modifier.fillMaxWidth()
+                .height(400.dp),
+                shape = RoundedCornerShape(12.dp)
+        ) {
+            characterOfTheDay?.let {
+                Box(contentAlignment = Alignment.BottomStart) {
+                    Box(
+                        Modifier.fillMaxSize()
+                            .background(Color.Green.copy(alpha = 0.5f))
+                    )
+                    Box(
+                        Modifier.fillMaxSize()
+                            .background(
+                                Brush.horizontalGradient(
+                                    0f to Color.Black.copy(alpha = 0.9f),
+                                    0.4f to Color.Transparent,
+                                    1f to Color.Black.copy(alpha = 0f)
+                                )
+                            )
+                    )
+                    Text(
+                        characterOfTheDay.name,
+                        fontSize = 40.sp,
+                        maxLines = 1,
+                        minLines = 1,
+                        textAlign = TextAlign.Center,
+                        color = Color.White,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .padding(horizontal = 24.dp, vertical = 16.dp)
+                            .fillMaxHeight()
+                            .vertical()
+                            .rotate(-90f)
+                    )
+
+                }
+            } ?: run {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+        }
+
+}
+
 /*
     * The collectAsState() function is the original way to collect a Flow or StateFlow inside a composable.
     * It allows you to convert the flow’s emitted values into Compose’s State type, which makes the values
