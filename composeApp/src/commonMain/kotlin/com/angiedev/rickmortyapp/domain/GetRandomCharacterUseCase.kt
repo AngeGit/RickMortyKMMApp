91a1fab93631f5167baabceeed5115201af82b4a
Angie <angelesmafon@gmail.com>
2025-01-28 19:37:36 +0100

    wip. compila.

--- composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/domain/GetRandomCharacterUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/domain/GetRandomCharacterUseCase.kt
@@ -1,12 +1,24 @@
 package com.angiedev.rickmortyapp.domain
 
+import androidx.compose.ui.input.key.Key.Companion.Calendar
 import com.angiedev.rickmortyapp.domain.model.CharacterModel
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacterUseCase(
     private val repository: Repository
 ) {
     suspend operator fun invoke(): CharacterModel {
+
         val randomId: Int = (1..826).random()
         return repository.getCharacter(randomId)
+    }
+    private fun getCurrentDayOfTheYear(): String {
+       val instant: Instant = Clock.System.now()
+       val localTime: LocalDateTime= instant.toLocalDateTime(TimeZone.currentSystemDefault())
+        return "${localTime.dayOfYear}${localTime.year}"
     }
 }
