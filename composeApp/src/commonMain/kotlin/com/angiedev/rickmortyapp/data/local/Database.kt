51559ae810e5a9ab831cec88012599c8d4cd815c
Angie <angelesmafon@gmail.com>
2025-01-29 17:41:14 +0100

    wip.Room working

---
+++ composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/data/local/Database.kt
@@ -0,0 +1,17 @@
+package com.angiedev.rickmortyapp.data.local
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import com.angiedev.rickmortyapp.data.local.entities.CharacterOfTheDayEntity
+
+const val DATABASE_NAME = "rickmorty_app.db"
+
+expect object RickMortyCTor: RoomDatabaseConstructor<RickMortyDatabase>
+
+@Database(entities = [CharacterOfTheDayEntity::class], version = 1)
+@ConstructedBy(RickMortyCTor::class)
+abstract class RickMortyDatabase:RoomDatabase() { //aquí van los dao
+    //abstract fun characterDao(): CharacterDao
+}
