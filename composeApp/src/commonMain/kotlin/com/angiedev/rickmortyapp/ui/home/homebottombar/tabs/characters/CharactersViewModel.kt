1aa1dfe74692d05183a4800408b6801c72ef5de8
Angie <angelesmafon@gmail.com>
2025-01-29 20:14:06 +0100

    refactor and fixing Pagging3 losing list stat

--- composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/ui/home/homebottombar/tabs/characters/CharactersViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/angiedev/rickmortyapp/ui/home/homebottombar/tabs/characters/CharactersViewModel.kt
@@ -2,8 +2,9 @@ package com.angiedev.rickmortyapp.ui.home.homebottombar.tabs.characters
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.angiedev.rickmortyapp.domain.GetAllCharactersUseCase
-import com.angiedev.rickmortyapp.domain.GetRandomCharacterUseCase
+import com.angiedev.rickmortyapp.domain.GetCharacterOfTheDayUseCase
 import com.angiedev.rickmortyapp.domain.model.CharacterModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -14,24 +15,29 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class CharactersViewModel(
-    private val getRandomCharacterUseCase: GetRandomCharacterUseCase,
+    private val getCharacterOfTheDayUseCase: GetCharacterOfTheDayUseCase,
     private val getAllCharacters: GetAllCharactersUseCase
 ): ViewModel(){
 
     private val _characterOfTheDayState = MutableStateFlow(CharacterState())
     val characterOfTheDayState: StateFlow<CharacterState> = _characterOfTheDayState
 
-    init { //Not sure it correct to use init here. Maybe this should be in a function called on the onViewCreated()
+    init {
         viewModelScope.launch {
             val result: CharacterModel = withContext(Dispatchers.IO){
-                getRandomCharacterUseCase()
+                getCharacterOfTheDayUseCase()
             }
             _characterOfTheDayState.update { it.copy(characterOfTheDay = result) }
         }
         fetchAllCharacters()
     }
-    private fun fetchAllCharacters() {
-        _characterOfTheDayState.update { it.copy(characters = getAllCharacters()) }
-    }
 
+    private fun fetchAllCharacters() =
+        //cachedIn(viewModelScope) is't a recomendation of Google because Pagging3 library lose the state for a while
+        // and we lose the position of the list. With that we don't lose the state of the list
+        _characterOfTheDayState.update {
+            it.copy(
+                characters = getAllCharacters().cachedIn(viewModelScope)
+            )
+        }
 }
